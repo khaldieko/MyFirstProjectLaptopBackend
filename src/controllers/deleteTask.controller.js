7a97f3e2fa22ca1a46b956dd179fcfaaf77e4a0d
adepeju4 <adepeju.kayode@gmail.com>
2022-01-18 19:16:50 +0100

    fix node_modules

---
+++ src/controllers/deleteTask.controller.js
@@ -0,0 +1,23 @@
+const Task = require("../model/task.model.js");
+
+async function deleteTask(req, res) {
+  const { id } = req.query;
+  try {
+    if (id) {
+      const deleteTask = await Task.findByIdAndDelete({ _id: id });
+      if (deleteTask) {
+        return res.status(200).json({ success: true, ...deleteTask });
+      }
+      return res
+        .status(400)
+        .json({ success: false, message: "invalid request" });
+    }
+    return res.status(400).json({ success: false, message: "invalid request" });
+  } catch (error) {
+    return res
+      .status(500)
+      .json({ success: false, message: "internal server error" });
+  }
+}
+
+module.exports = deleteTask;
